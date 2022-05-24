5fe8922727a46a625d9f44aa6c5a9f96d51c2ffb
Dominik Drozd <dominik_drozd@vp.pl>
2022-05-24 19:18:36 +0200

    Add custom error class and errors handling.

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
+import {handleError, handleNotFound } from './utils/error';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(handleNotFound);
+
+app.use(handleError);
 
 app.listen(3000, '127.0.0.1', () => {
     console.log('Server is running on port 3000');
